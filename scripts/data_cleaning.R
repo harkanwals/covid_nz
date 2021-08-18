449175670a4eb622bc1642904f950a460f0013ba
Harkanwal <harkanwal@fastmail.fm>
2021-08-18 23:32:03 +1200

    Fix dates

--- scripts/data_cleaning.R
+++ scripts/data_cleaning.R
@@ -9,14 +9,13 @@ cases <- read_csv("raw_data/covid_cases_2021-08-18.csv") %>%
 cases %>% 
   filter(dhb != "Managed Isolation & Quarantine") %>% 
   group_by(report_date) %>% 
-  count(name = "case_count") %>% 
+  count(name = "case_count") %>% View()
   write_csv("data/community_case_count.csv")
 
 cases %>% 
   filter(dhb == "Managed Isolation & Quarantine") %>% 
   group_by(report_date) %>% 
-  count()  %>% 
-  count(name = "case_count")  %>% 
+  count(name = "case_count")  %>%
   write_csv("data/miq_case_count.csv")
 
 
@@ -24,7 +23,19 @@ cases %>%
 scans <- read_csv("raw_data/nz-covid-tracer-usage-2021-08-18.csv") %>% 
   clean_names()
 
-scans %>% 
-  mutate(origin_date = as.Date(gsub(" 12:00","", date_time_from, fixed = TRUE))) %>% 
-  write_csv("data/scans_count.csv")
+scans[is.na(scans)] = 0
 
+
+scans %>% 
+  mutate(origin_date = as.Date(gsub(" 12:00","", date_time_from, fixed = TRUE), format= "%d/%m/%y")) ->
+  scans
+
+scans %>% select(origin_date, app_registrations, nzbn_registered_businesses) %>% 
+  write_csv("data/tracer_registrations.csv")
+
+scans %>% select(origin_date, scans, manual_entries) %>% 
+  write_csv("data/tracer_scans.csv")
+
+scans %>% 
+  select(origin_date, active_devices, bluetooth_active_24hr) %>% 
+  write_csv("data/tracer_actives.csv")
